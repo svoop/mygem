f5204f1b8dc50b8989f057d4da5fedbf634b7dfa
Sven Schwyn <sven.schwyn@bitcetera.com>
2023-04-21 17:18:50 +0200

    Example

---
+++ vendor/gems/myvendoredgem/lib/myvendoredgem.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "myvendoredgem/version"
+
+module Myvendoredgem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ vendor/gems/myvendoredgem/test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "myvendoredgem"
+
+require "minitest/autorun"
